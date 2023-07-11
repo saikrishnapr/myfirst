bdd807ce24269fe0523fcb2f7b429e048550fb7d
saikrishnapr <107532537+saikrishnapr@users.noreply.github.com>
2023-07-11 18:49:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+'''
+  This is the config file which includes all ccnfiguation info here
+'''
+
+_DB_CONF = {
+ 'host':'172.168.64.1',
+ 'port':3306,
+ 'user':'sk',
+ 'passwd':'Learn@123',
+ 'db':'cafe'
+}
